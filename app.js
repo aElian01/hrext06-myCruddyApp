127cc8f7b31c83381f33f27c629e3c24c47e0215
Eliana Sur Under <elianasurUnder@Elianas-Air.localdomain>
2018-12-18 23:15:07 -0800

    implementation for editing local storage to include more than one value, deleting value

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 $(document).ready(function(){
   console.log('jQuery loaded');
 
-  // write to local storage from input when button save clicked
+  // write to local storage from input when button save clicked; store multiple
   $('.btn-submit').on('click', function(){
-    localStorage.setItem('inputFieldValue', $('.text-entry').val());
-    var myItemInStorage = localStorage.getItem('inputFieldValue');
+    var inputFieldValue = localStorage.getItem('inputFieldValue');  
+    if (inputFieldValue === null) {
+      var inputFieldValuesInArr = [];
+    } else {
+      inputFieldValuesInArr = JSON.parse(inputFieldValue);
+    }
+    inputFieldValuesInArr.push($('.text-entry').val());
+    localStorage.setItem('inputFieldValue', JSON.stringify(inputFieldValuesInArr));
+    var myItemInStorage = JSON.parse(localStorage.getItem('inputFieldValue'));
     console.log('myItemInStorage', myItemInStorage);
-
+    
     // display the value here
     $('.list-display-field').text(myItemInStorage); // ??
-
-  });
+  }); 
 
   // delete from local storage when delete button clicked
   $('.btn-delete').on('click', function(){
-    localStorage.removeItem('inputFieldValue');
+    var inputFieldValue = JSON.parse(localStorage.getItem('inputFieldValue'));
+    if (inputFieldValue !== null) {
+      if (inputFieldValue.includes($('.text-entry').val())) {
+        var startIndex = inputFieldValue.indexOf($('.text-entry').val())
+        inputFieldValue.splice(startIndex, 1);
+        localStorage.setItem('inputFieldValue', JSON.stringify(inputFieldValue));  
+      }
+    }
+    var myItemInStorage = JSON.parse(localStorage.getItem('inputFieldValue'));
+    $('.list-display-field').text(myItemInStorage); 
   });
 
 });
